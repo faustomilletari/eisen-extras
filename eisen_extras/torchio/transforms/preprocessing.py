88265b7b47845bd93db186c40f02a971e3db7cbb
Fausto Milletari <fausto.milletari@gmail.com>
2020-07-07 09:29:14 +0200

    more pre-processing tform

--- eisen_extras/torchio/transforms/preprocessing.py
+++ eisen_extras/torchio/transforms/preprocessing.py
@@ -6,6 +6,12 @@ from torchio.transforms import RescaleIntensity as TorchIORescaleIntensity
 from torchio.transforms import ZNormalization as TorchIOZNormalization
 from torchio.transforms import HistogramStandardization as TorchIOHistogramStandardization
 
+from torchio.transforms import CropOrPad as TorchIOCropOrPad
+from torchio.transforms import Crop as TorchIOCrop
+from torchio.transforms import Pad as TorchIOPad
+from torchio.transforms import Resample as TorchIOResample
+from torchio.transforms import ToCanonical as TorchIOToCanonical
+
 
 class RescaleIntensity:
     def __init__(
@@ -39,17 +45,39 @@ class RescaleIntensity:
 
 
 class ZNormalization:
+    """
+    Performs ZNormalization using TorchIO. This transformation performs the same functionality as
+    torchio.transforms.ZNormalization but exposes an interface compatible with Eisen and its functionality
+    uses the data processing paradigm employed in Eisen.
+    """
     def __init__(
             self,
             fields: List[str],
             mask_field: str = None,
             p: float = 1,
     ):
+        """
+        Initializes an object of type eisen_extras.torchio.transforms.ZNormalization
+
+        :param fields:
+        :type fields: list of str
+        :param mask_field: The field of the data dictionary containing a tensor to be used as mask. Can be None
+        :type mask_field: str
+        :param p: probability of applying this transform
+        :type p: float
+        """
         self.fields = fields
         self.mask_field = mask_field
         self.tform = TorchIOZNormalization(mask_field, p)
 
     def __call__(self, data: dict) -> dict:
+        """
+        Runs the transform on the data dictionary.
+
+        :param data: dictionary containing data
+        :type data: dict
+        :return: dictionary containing transformed data
+        """
         for field in self.fields:
             if self.mask_field is None:
                 mask = torch.ones(*data[field].shape).bool()
@@ -70,7 +98,51 @@ class HistogramStandardization:
     def __init__(
             self,
             fields: List[str],
+            histogram_field: str,
             mask_field: str = None,
             p: float = 1,
     ):
+        self.fields = fields
+        self.histogram_field = histogram_field
+        self.mask_field = mask_field
+
+        self.tform = TorchIOHistogramStandardization(
+            landmarks={},  # just for init
+            masking_method=mask_field,
+            p=p
+        )
+
+    def __call__(self, data):
+        for field in self.fields:
+            if self.mask_field is None:
+                mask = torch.ones(*data[field].shape).bool()
+            else:
+                mask = torch.from_numpy(data[self.mask_field]).bool()
+
+            data_tensor = self.tform.normalize(
+                tensor=torch.from_numpy(data[field]),
+                landmarks=data[self.histogram_field],
+                mask=mask
+            )
+
+            data[field] = data_tensor.numpy()
+
+        return data
+
+
+class CropOrPad:
+    def __init__(
+            self,
+            fields: List[str],
+            target_shape: int,
+            padding_mode: str,
+            mask_field: str = None,
+            p: float = 1,
+    ):
+        self.fields = fields
+        self.mask_field = mask_field
+        self.tform = TorchIOCropOrPad(target_shape, padding_mode, mask_field, p)
+
+    def __call__(self, data: dict) -> dict:
         pass
+
